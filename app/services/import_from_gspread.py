c44429c93f3d19c8d629284cf3d4939808817985
Luis Milanese <luis.milanese@jacotei.com.br>
2019-04-12 12:02:20 -0300

    google spreadsheet importer service

--- app/services/import_from_gspread.py
+++ app/services/import_from_gspread.py
@@ -1,7 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from app import app
-from app.models.album import Album
+from app import app, db
+from app.models.album import Album, Artist
 
 
 class Importer:
@@ -9,13 +9,31 @@ class Importer:
         albums = self._read_from_gspread()
         for album in albums:
             a = Album(title=album['title'], personal_note=album['personal_notes'], wish_list=album['wish_list'])
-            if not self._check_album_already_exists(album=a):
-                print('Não existe!')
+            if not self._album_already_exists(album=a):
+                artists = self._extract_artists_from_album(album_artists=album['artists'])
+                for artist in artists:
+                    a.artists.append(artist)
+                db.session.add(a)
+                db.session.commit()
             else:
                 print('Já existe')
 
-    def _check_album_already_exists(self, album):
+    def _album_already_exists(self, album):
         return Album.query.filter_by(title=album.title).first()
+
+    def _extract_artists_from_album(self, album_artists):
+        artists_names = album_artists.split(";")
+        artists = []
+        for artist_name in artists_names:
+            artist = Artist.query.filter_by(name=artist_name).first()
+            if not artist:
+                a = Artist(name=artist_name)
+                artists.append(a)
+                db.session.add(a)
+            else:
+                artists.append(artist)
+
+        return artists
 
     def _read_from_gspread(self):
         scope = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
